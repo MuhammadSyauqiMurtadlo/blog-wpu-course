3f20ae1c37d991aa40c355d610c474e679a90237
Syauqi <sauki084@gmail.com>
2026-02-24 22:22:46 +0700

    Blog Post Page: Send array data from route to blade

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
 });
-Route::get('/blog', function() {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function() {
+    $posts = [
+        [
+            'title' => 'Judul Post 1',
+            'author' => 'Muhammad Syauqi Murtadlo',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum accusamus beatae dolor, recusandae ex in itaque ratione deleniti id aliquam voluptates aspernatur quod nihil at praesentium quia reiciendis! Molestias, esse.'
+        ],
+        [
+            'title' => 'Judul Post 2',
+            'author' => 'Kevin McAllister',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus et minima quis quibusdam impedit eaque magni obcaecati numquam earum unde facilis quasi, accusantium quam repellat recusandae autem commodi error fugiat.'
+        ]
+    ];
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 Route::get('/about', function () {
     return view('about', ['title' => 'About']);
