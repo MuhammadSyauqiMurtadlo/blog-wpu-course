6581893a55ebbbf9dfa640c035fe2312a641a405
Syauqi <sauki084@gmail.com>
2026-03-05 08:46:19 +0700

    Route Model Binding

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,8 @@ Route::get('/posts', function() {
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
-Route::get('/posts/{slug}', function($slug) {
-    $post = Post::find($slug);
-    return view('post', ['title' => $post['title'], 'post' => $post]);
+Route::get('/posts/{post:slug}', function(Post $post) {
+    return view('post', ['title' => $post->title, 'post' => $post]);
 });
 
 Route::get('/about', function () {
